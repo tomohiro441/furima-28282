e7fcb1694c250e4689f0df2d4422038cb175a9fd
tomohiro441 <metype441wetype@gmail.com>
2020-08-08 17:32:00 +0900

    モデル単体テストコードの実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,13 @@
-factory :user do
-  nickname              {"furima太郎"}
-  email                 {"kkk@gmail.com"}
-  password              {"00000000"}
-  password_confirmation {password}
-  last_name             {"太郎"}
-  first_name            {"山本"}
-  last_name_reading     {"タロウ"}
-  first_name_readin     {"ヤマモト"}
-  birthday              {"1990, 07, 07"}
-end
-end
+FactoryBot.define do
+  factory :user do
+    nickname              {"furima太郎"}
+    email                 {"kkk@gmail.com"}
+    password              {"00000000"}
+    password_confirmation {password}
+    last_name             {"太郎"}
+    first_name            {"佐藤"}
+    last_name_reading     {"タロウ"}
+    first_name_reading    {"サトウ"}
+    birthday              {"1990, 07, 07"}
+  end
+end  
