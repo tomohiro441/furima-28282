6a1d81dfe3ea987f2e5e76f485718e4102c3cd1b
tomohiro441 <metype441wetype@gmail.com>
2020-08-08 12:29:59 +0900

    FactoryBot,fake導入

---
+++ spec/factories/users.rb
@@ -0,0 +1,12 @@
+factory :user do
+  nickname              {"furima太郎"}
+  email                 {"kkk@gmail.com"}
+  password              {"00000000"}
+  password_confirmation {password}
+  last_name             {"太郎"}
+  first_name            {"山本"}
+  last_name_reading     {"タロウ"}
+  first_name_readin     {"ヤマモト"}
+  birthday              {"1990, 07, 07"}
+end
+end
