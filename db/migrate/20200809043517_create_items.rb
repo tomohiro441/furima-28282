830e87ac5eeacfe2b2563d1b6994ad4574c8f862
tomohiro441 <metype441wetype@gmail.com>
2020-08-09 13:54:42 +0900

    マイグレーションの実行

--- db/migrate/20200809043517_create_items.rb
+++ db/migrate/20200809043517_create_items.rb
@@ -1,7 +1,14 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string  :name
+      t.text :info
+      t.integer :price 
+      t.integer :category_id 
+      t.integer :delivery_fee_id
+      t.integer :delivery_region_id 
+      t.integer :delivery_date_id
+      t.references :user
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_06_080033) do
+ActiveRecord::Schema.define(version: 2020_08_09_043517) do
 
   create_table "installs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,20 @@ ActiveRecord::Schema.define(version: 2020_08_06_080033) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_installs_on_email", unique: true
     t.index ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true
+  end
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.text "info"
+    t.integer "price"
+    t.integer "category_id"
+    t.integer "delivery_fee_id"
+    t.integer "delivery_region_id"
+    t.integer "delivery_date_id"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
