cd7629c086e2ffee5553eefc4efc4397d20733cc
tomohiro441 <metype441wetype@gmail.com>
2020-08-11 17:14:06 +0900

    マイグレーションファイル（users）の修正

--- db/schema.rb
+++ db/schema.rb
@@ -12,18 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_08_06_080033) do
 
-  create_table "installs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_installs_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
@@ -32,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_08_06_080033) do
     t.string "first_name", default: "", null: false
     t.string "last_name_reading", default: "", null: false
     t.string "first_name_reading", default: "", null: false
-    t.string "birthday", default: "", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
