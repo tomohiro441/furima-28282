c6ce12041b9a1e4f6c5421c3eec96b5bc25ff110
tomohiro441 <metype441wetype@gmail.com>
2020-08-15 22:48:26 +0900

    itemモデル単体テストコードの記載

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  
+  describe 'アイテムが保存できない場合' do
+    it "画像が空だと登録できない" do
+      @item.image = nil
+      @item.valid?  
+      expect(@item.errors.full_messages).to include("Image can't be blank")   
+    end 
+    it "商品名が空だと登録できない" do
+      @item.name =""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")   
+    end  
+    it "商品の説明が空だと登録できない" do
+      @item.info =""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Info can't be blank")  
+    end  
+    it "カテゴリーの情報が空だと登録できない" do
+      @item.category_id =""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category can't be blank")  
+    end  
+    it "商品の状態が選択されていないと登録できない" do
+      @item.condition_id = nil 
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Condition can't be blank")
+    end    
+    it "配送料の負担についての情報が選択されていないと登録できない" do
+      @item.delivery_fee_id = nil 
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery fee can't be blank") 
+    end 
+    it "発送元の地域についての情報が選択されていないと登録できない" do
+      @item.delivery_region_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery region can't be blank") 
+    end  
+    it "発送までの日数についての情報が選択されていないと登録できない" do
+      @item.delivery_date_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Delivery date can't be blank") 
+    end  
+    it "価格についての情報が選択されていないと登録できない"do
+      @item.price = nil 
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank") 
+    end  
+    it "価格の範囲が、¥300以下だと登録できない" do
+      @item.price = 299 
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300") 
+    end
+    it "価格の範囲が、¥9999999を超えると登録できない" do
+      @item.price = 10000000
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999") 
+    end  
+  end    
 end
