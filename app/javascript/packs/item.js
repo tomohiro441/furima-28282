136e8a11095c89a469f068370ab3781a871506f6
tomohiro441 <metype441wetype@gmail.com>
2020-08-15 22:45:01 +0900

    価格関連（JSを用いた非同期表示部分）実装

--- app/javascript/packs/item.js
+++ app/javascript/packs/item.js
@@ -1,1 +1,11 @@
-console.log("OK")
+  function price() {
+  const input = document.getElementById("item-price")
+  input.addEventListener('input', function(){ 
+    const item_price = document.getElementById("item-price").value;
+    const add_tax_price = item_price * 0.1;
+    const profit = item_price - add_tax_price;
+    document.getElementById("add-tax-price").innerHTML = add_tax_price;
+    document.getElementById("profit").innerHTML = profit
+  })
+}
+window.addEventListener("load", price);
