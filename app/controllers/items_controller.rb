e0c339d77932d9d881315055c7b07d402c4b0421
tomohiro441 <metype441wetype@gmail.com>
2020-08-18 19:26:23 +0900

    itemコントローラの一部修正（２）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,11 +20,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to show
     else
@@ -33,7 +31,6 @@ class ItemsController < ApplicationController
   end
     
   def show
-      @item = Item.find(params[:id])
   end
 
   private
