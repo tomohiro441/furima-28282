d610c134e2495fdc5ff7c15e682341f774b21244
tomohiro441 <metype441wetype@gmail.com>
2020-08-18 17:41:23 +0900

    edit・upadateアクションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,15 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+  end
+
   private
 
   def redirect_user_session_path
